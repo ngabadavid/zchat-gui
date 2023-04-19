6a8cf5978617244f47174f2732c7040e8c8445e3
ngabadavid <107659284+ngabadavid@users.noreply.github.com>
2023-04-19 13:28:32 -0400

    Initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/zchat-ui'),
+      dir: require('path').join(__dirname, './coverage/zchat-gui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'zchat-ui'`, () => {
+  it(`should have as title 'zchat-gui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('zchat-ui');
+    expect(app.title).toEqual('zchat-gui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('zchat-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('zchat-gui app is running!');
   });
 });
